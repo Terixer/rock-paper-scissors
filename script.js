c82c0a13b8762a13677d61a9391832c2502983c3
Kamil Bartczak <kontakt@kamilbartczak.pl>
2018-12-06 16:50:34 +0100

    style all html

---
+++ script.js
@@ -0,0 +1,31 @@
+const page = {
+    score: {
+        computer: "computer-score",
+        user: "user-score"
+    },
+    result: {
+        verdict: "result-verdict",
+        computerChoise: "computer-choice",
+        userChoise: "user-choice",
+    },
+    choice: {
+        choiceClass:"single-choice",
+        choiceAttr:"data-choice"
+    }
+}
+let score = {
+    user: 0,
+    computer: 0
+}
+
+// function main() {
+//     document.getElementById("myBtn").addEventListener("click", function () {
+//         document.getElementById("demo").innerHTML = "Hello World";
+//     });
+// }
+
+// console.table(page);
+var imie ="KAMIL";
+var nazwisko ="BAŁA";
+
+console.log(`${imie} ${nazwisko}`);
